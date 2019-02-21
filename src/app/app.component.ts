f39e3582480f28b2f45d4882de51db379db87cd3
alessandro161 <alessandrogasparini1995@gmail.com>
2019-02-21 16:48:19 +0100

    notizie API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,3 +8,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'notizie';
 }
+$(function(){
+  $.ajax({
+   type:'GET',
+   url:"https://newsapi.org/v2/top-headlines?sources=talksport&apiKey=dcbe67b159424c85be9e1745d5c5fdcb",
+   data:'html',
+   success: function(result){
+      for(var i=0;i<result.totalResults;i++){
+        $("table").append("<tr> <td>"+(result.articles[i].author)+"</td>"+"<td>"+(result.articles[i].title)+"</td>"+"<td>"+(result.articles[i].description)+"</td>");
+                                            }
+      $(function(){
+                    $('td').mouseover(function(){
+                   $(this).animate({ fontSize: "25px"},3000) ;     
+                                                });
+                    $('td').mouseout(function(){
+                    $(this).animate({ fontSize: "11px"},3000);
+                                                });
+                    });
+   },
+   error: function(xhr){
+   console.log("error")
+   },
+  });
+  });
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+};
+  };
+     
+     
